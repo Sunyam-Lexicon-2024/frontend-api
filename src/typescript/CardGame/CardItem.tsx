9b096277134d229aae0200af073a10a249b6e056
suny-am <visualarea.1@gmail.com>
2024-04-03 21:29:15 +0200

    Make site responsive

--- src/typescript/CardGame/CardItem.tsx
+++ src/typescript/CardGame/CardItem.tsx
@@ -1,17 +1,18 @@
 export default function CardItem({ card }: CardProps) {
 	return (
-		<div className="api-item">
-			<p className="api-item-description-header">Card Description</p>
-			<p className="api-item-description">
-			{card.value.toLowerCase()} of {card.suit.toLowerCase()} 
-			</p>
-			{
-				<img
-					className="api-item-image"
-					src={card.image}
-					alt={`${card.suit}-${card.value}`}
-				/>
-			}
+		<div className="api-item flex-row">
+			<div>
+				<p className="api-item-description-header">Card Description</p>
+				<p className="api-item-description">
+					{card.value.toLowerCase()} of {card.suit.toLowerCase()}
+				</p>
+			</div>
+
+			<img
+				className="api-item-image"
+				src={card.image}
+				alt={`${card.suit}-${card.value}`}
+			/>
 		</div>
 	)
 }
